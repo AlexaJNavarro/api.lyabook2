6070f336ec1c7451ddd6e14a4c2a885d45a0fb53
Alexandra Jaqueline Navarro Navarro <alexa@pop-os.localdomain>
2021-08-19 00:05:25 -0500

    list category

--- src/model/category.model.ts
+++ src/model/category.model.ts
@@ -1,16 +1,8 @@
 import categoryEntity from '../entity/category.entity'
 
 export class CategoryModel{
-    public static async GetAll(){
-        // const category = categoryEntity.find()
-        // return category
-        const data = new categoryEntity()
-        data.name = "ale"
-        data.slug = "ale_tu_amorcito"
-        data.active= true
-        const category = categoryEntity.create(data)
-        console.log(category)
-
-    
+    public static GetAll(){
+        const category = categoryEntity.find()
+        return category
     }
 }
