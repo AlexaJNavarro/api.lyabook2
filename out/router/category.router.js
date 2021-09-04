afaa6af79326fc72c094524aa8fc756539054c24
Alexandra Jaqueline Navarro Navarro <alexa@pop-os.localdomain>
2021-09-04 18:04:23 -0500

    middleware validation finish

--- out/router/category.router.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var category_controller_1 = require("../controller/category.controller");
-var routeCategory = express_1.Router()
-    .get('/categories', category_controller_1.CategoryController.GetAll);
-exports.default = routeCategory;
